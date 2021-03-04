96bd53441784b88f08b9dcd3129a41e97a4d4913
Emmanuel <github-sub@shedge.com>
2021-03-04 16:28:25 +0100

    feat: add Client model to the admin

--- apps/crm/admin.py
+++ apps/crm/admin.py
@@ -1,3 +1,48 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Client, Contract, Event
+
+# admin.site.register(Client)
+admin.site.register(Contract)
+admin.site.register(Event)
+
+
+@admin.register(Client)
+class ClientAdmin(admin.ModelAdmin):
+    """ Define the 'Client' admin section behaviors & displays. """
+
+    # inlines = [UserContribInline, UserIssuesInline]
+
+    fieldsets = [
+        (
+            None,
+            {
+                "fields": [
+                    "compagny_name",
+                    "status",
+                    "sales_contact",
+                    "contact_first_name",
+                    "contact_last_name",
+                    "contact_email",
+                    "contact_mobile",
+                    "company_phone",
+                ]
+            },
+        ),
+        (
+            "Dates",
+            {
+                "fields": ["created_time", "updated_time"],
+            },
+        ),
+    ]
+
+    readonly_fields = ["created_time", "updated_time"]
+
+    list_display = (
+        "compagny_name",
+        "status",
+        "sales_contact",
+    )
+
+    list_filter = ("status", "sales_contact")
