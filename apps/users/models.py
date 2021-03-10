4b0743be1218447f9a3cab337a9da97db5bf7b64
Emmanuel <github-sub@shedge.com>
2021-03-10 22:46:36 +0100

    refactor: update the EpicMember model in order to make some fields required or not & migrates

--- apps/users/models.py
+++ apps/users/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser  # , BaseUserManager
 
 
 class EpicMember(AbstractUser):
@@ -9,9 +9,10 @@ class EpicMember(AbstractUser):
         SUPPORT = "SUPPORT", "Support team"
         UNDEFINED = "NONE", "Need a team"
 
-    first_name = models.CharField("First name", max_length=25)
-    last_name = models.CharField("Last name", max_length=25)
-    email = models.EmailField("Email", max_length=100)
+    first_name = models.CharField("First name", max_length=25, null=True, blank=True)
+    last_name = models.CharField("Last name", max_length=25, null=True, blank=True)
+    email = models.EmailField("Email", max_length=100, null=True, blank=True)
+    password = models.CharField("Password", max_length=100, null=False, blank=False)
 
     team = models.CharField(
         "Team", max_length=10, choices=Team.choices, default=Team.UNDEFINED
