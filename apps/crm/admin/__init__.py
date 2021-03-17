fb74f5b4ea4c2eff00c67da264a14850d847c52d
Emmanuel <github-sub@shedge.com>
2021-03-17 11:42:11 +0100

    refactor: add __all__ to admin/__ini__.py and filters/__init___.py so Flake8 doesn't return warnings

--- apps/crm/admin/__init__.py
+++ apps/crm/admin/__init__.py
@@ -2,3 +2,5 @@ from apps.crm.admin.status import StatusAdmin
 from apps.crm.admin.client import ClientAdmin
 from apps.crm.admin.contract import ContractAdmin
 from apps.crm.admin.event import EventAdmin
+
+__all__ = ['StatusAdmin', 'ClientAdmin', 'ContractAdmin', 'EventAdmin', ]
--- apps/crm/filters/__init__.py
+++ apps/crm/filters/__init__.py
@@ -1,3 +1,5 @@
 from apps.crm.filters.client import ClientFilter
 from apps.crm.filters.contract import ContractFilter
 from apps.crm.filters.event import EventFilter
+
+__all__ = ['ClientFilter', 'ContractFilter', 'EventFilter', ]
